30cdfa9541ff8037a469450f7fff04827dcc7f08
Robert Ringeling <robert.ringeling@quotesoft.com>
2019-03-22 15:11:25 -0700

    Create new GradeBooks
    Created GradeBookType enum.
    Added Type property to BaseGradeBook.
    Created StandardGradeBook.
    Created RankedGradeBook.

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Linq;
-
-using GradeBook.Enums;
-using System.Collections.Generic;
-using System.IO;
+﻿using GradeBook.Enums;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -13,6 +12,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; set; }
 
         public BaseGradeBook(string name)
         {
